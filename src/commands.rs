3b66092112ffacc14ebe12a63fbb116973535be0
castroploiin <fieryfire1005@gmail.com>
2023-05-25 16:05:35 +0400

    Added: user input function

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,7 @@
-use serenity::model::prelude::{Embed, Message};
+use std::time::Duration;
+use serenity::model::prelude::{Message};
 use serenity::prelude::Context;
-use serenity::utils::MessageBuilder;
+use serenity::utils::{MessageBuilder, Color};
 
 pub async fn pong(ctx: Context, message: Message) {
     let mut pong_buildup = MessageBuilder::new();
@@ -13,11 +14,26 @@ pub async fn pong(ctx: Context, message: Message) {
 }
 
 pub async fn hello_embed(ctx: Context, message: Message) {
-    let embed = Embed::fake(|e| e.title("Hello!").description("Testing"));
     message
         .channel_id
-        .say(&ctx.http, embed)
+        .send_message(&ctx.http, |msg| {
+            msg.embed(|field| {
+                field.title("Hello world");
+                field.color(Color::from_rgb(255, 0, 255))
+            })
+        })
         .await
         .expect("Could not send message");
 }
 
+pub async fn get_input(ctx: Context, message: Message) {
+    const TIMEOUT_DURATION: Duration = Duration::from_secs(5);
+    let _ = message.reply(&ctx.http, "Say something").await;
+
+    if let Some(answer) = &message.author.await_reply(&ctx).timeout(TIMEOUT_DURATION).await {
+        let user_reply = format!("YOU SAID {}", &answer.content);
+        answer.reply(&ctx, user_reply).await.expect("Error parroting back user message");
+    } else {
+        message.channel_id.say(&ctx.http, "You did not send a message in 3 seconds").await.expect("Error sending timed out message");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,26 @@
-use serenity::model::prelude::{ChannelId, Message, Ready};
-use serenity::utils::MessageBuilder;
+use serenity::model::prelude::{Message, Ready};
+
 use serenity::{async_trait, prelude::*};
 use serenitybot::commands;
 
-const PREFIX: &str = "?";
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, ctx: Context, ready: Ready) {
-        println!("{} is online", ready.user.name);
-        let general_chat = ChannelId(1110107351287087227);
-        let message = MessageBuilder::new().push("Hello World.").build();
-        general_chat
-            .say(&ctx.http, message)
-            .await
-            .expect("Could not send message");
-    }
+    async fn ready(&self, _: Context, _: Ready) {
+        println!("Online");
+}
 
-    async fn message(&self, ctx: Context, message: Message) {
-        for command in message.content.split(PREFIX) {
-            match command {
-                "ping" => commands::pong(ctx.clone(), message.clone()).await,
-                "embed" => commands::hello_embed(ctx.clone(), message.clone()).await,
-                _ => (),
-            }
+async fn message(&self, ctx: Context, message: Message) {
+        const PREFIX: &str = "?";
+
+        let command = &message.content.replace(PREFIX, "")[..];
+        match command {
+            "ping" => commands::pong(ctx.clone(), message.clone()).await,
+            "embed" => commands::hello_embed(ctx.clone(), message.clone()).await,
+            "some" => commands::get_input(ctx.clone(), message.clone()).await,
+            _ => (),
         }
     }
 }
