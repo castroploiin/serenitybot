51044e677928c3a6cd4494a6ce142425491fcdd7
castroploiin <fieryfire1005@gmail.com>
2023-07-10 12:55:41 +0400

    Added: fix_deviants function

--- src/commands.rs
+++ src/commands.rs
@@ -1,15 +1,18 @@
 use rand::{thread_rng, Rng};
-use serenity::model::prelude::{Message, Member, UserId, ChannelId, GuildId};
-use serenity::prelude::{Context};
-use serenity::utils::{MessageBuilder, Color};
+// use serenity::framework::standard::{
+//     help_commands, Args, CommandGroup, CommandResult, HelpOptions,
+// };
+use serenity::model::prelude::{ChannelId, GuildId, Member, Message, UserId};
+use serenity::prelude::Context;
+use serenity::utils::{Color, MessageBuilder};
 use std::fs::File;
 use std::io::Read;
 use std::time::Duration;
 
 use crate::structures::CmdDocumentation;
 
-pub async fn pong(ctx: Context, message: Message) {
-    let mut pong_buildup: MessageBuilder = MessageBuilder::new(); 
+pub async fn pong(ctx: &Context, message: &Message) {
+    let mut pong_buildup: MessageBuilder = MessageBuilder::new();
     let real_pong: &mut MessageBuilder = pong_buildup.push("Pong!");
     message
         .channel_id
@@ -18,7 +21,7 @@ pub async fn pong(ctx: Context, message: Message) {
         .expect("Cannot send message");
 }
 
-pub async fn hello_embed(ctx: Context, message: Message) {
+pub async fn hello_embed(ctx: &Context, message: &Message) {
     message
         .channel_id
         .send_message(&ctx.http, |msg| {
@@ -31,7 +34,7 @@ pub async fn hello_embed(ctx: Context, message: Message) {
         .expect("Could not send message");
 }
 
-pub async fn get_input(ctx: Context, message: Message) {
+pub async fn get_input(ctx: &Context, message: &Message) {
     const TIMEOUT_DURATION: Duration = Duration::from_secs(3);
     let _ = message.reply(&ctx.http, "Say something").await;
 
@@ -61,14 +64,13 @@ pub async fn get_input(ctx: Context, message: Message) {
     }
 }
 
-pub async fn get_help(ctx: Context, message: Message) {
+pub async fn get_help(ctx: &Context, message: &Message) {
     let _ = message
         .reply(&ctx.http, "What command do you want help with?")
         .await;
 
     if let Some(answer) = &message.author.await_reply(&ctx).await {
-        let help: Result<String, serde_json::Error> =
-            fetch_help_information(&answer.content).await;
+        let help: Result<String, serde_json::Error> = fetch_help_information(&answer.content).await;
         match help {
             Ok(command) => {
                 answer
@@ -118,7 +120,7 @@ pub async fn fetch_help_information(command: &String) -> Result<String, serde_js
     Ok(String::from("Could not find command"))
 }
 
-pub async fn eightball(ctx: Context, message: Message) {
+pub async fn eightball(ctx: &Context, message: &Message) {
     let responses: Vec<&str> = vec!["yes", "no"];
     let num_responses: usize = responses.len();
 
@@ -130,31 +132,89 @@ pub async fn eightball(ctx: Context, message: Message) {
         .expect("Could not send 8ball message");
 }
 
-pub async fn scan_for_deviants(ctx: &Context, current_guild: GuildId) -> Vec<Member> {
+pub async fn scan_for_deviants(ctx: &Context, current_guild: &GuildId) -> Vec<Member> {
     let mut list_of_deviants: Vec<Member> = Vec::new();
-    let members: Vec<Member> = current_guild.to_partial_guild(ctx).await.expect("Did not get Guild from GuildId").members(ctx, None, UserId(00000000000000000000)).await.expect("");
+    let members: Vec<Member> = current_guild
+        .to_partial_guild(ctx)
+        .await
+        .expect("Did not get Guild from GuildId")
+        .members(ctx, None, UserId(00000000000000000000))
+        .await
+        .expect("");
 
     for member in members {
-        let has_role: &bool = &member.user.has_role(ctx, current_guild, 1126217605976436777).await.expect("Could not determine if user has role or not"); 
+        let has_role: &bool = &member
+            .user
+            .has_role(ctx, *current_guild, 1126217605976436777)
+            .await
+            .expect("Could not determine if user has role or not");
         if !has_role {
             list_of_deviants.push(member)
-        } 
+        }
     }
 
     list_of_deviants
 }
 
-pub async fn print_deviants(ctx: Context, channel_id: ChannelId, current_guild: GuildId) {
-    let mut message = MessageBuilder::new();
-    message.push("List of deviants: ");
+pub async fn print_deviants(
+    ctx: &Context,
+    channel_id: &ChannelId,
+    current_guild: &Option<GuildId>,
+) {
+    match current_guild {
+        Some(guild_id) => {
+            let mut message = MessageBuilder::new();
+            message.push("List of deviants: ");
 
-    let mut names_of_deviants: Vec<String> = Vec::new();
+            let mut names_of_deviants: Vec<String> = Vec::new();
 
-    let list_of_deviants = scan_for_deviants(&ctx, current_guild).await; 
-    for deviant in list_of_deviants {
-        names_of_deviants.push(deviant.user.name.to_string());
+            let list_of_deviants = scan_for_deviants(&ctx, guild_id).await;
+            for deviant in list_of_deviants {
+                names_of_deviants.push(deviant.user.name.to_string());
+            }
+
+            message.push(names_of_deviants.join(", "));
+            channel_id
+                .say(ctx, message)
+                .await
+                .expect("Could not send deviants list in channel");
+        }
+        _ => println!("No GuildId received in print_deviants"),
     }
+}
 
-    message.push(names_of_deviants.join(", "));
-    channel_id.say(ctx, message).await.expect("Could not send deviants list in channel");
+// #[help]
+// pub async fn my_help(
+//     ctx: &Context,
+//     message: &Message,
+//     args: Args,
+//     help_options: &'static HelpOptions,
+//     groups: &[&'static CommandGroup],
+//     owners: HashSet<UserId>,
+// ) -> CommandResult {
+//     let help = help_commands::with_embeds(ctx, message, args, help_options, groups, owners).await;
+//     help
+// }
+
+pub async fn fix_deviants(ctx: &Context, message: &Message, current_guild: &Option<GuildId>) {
+    match current_guild {
+        Some(guild_id) => {
+            let members: Vec<Member> = scan_for_deviants(&ctx, &guild_id).await;
+            for mut member in members {
+                member
+                    .add_role(&ctx, 1126217605976436777)
+                    .await
+                    .expect("Could not add role to deviint");
+            }
+
+            let mut complete_signal = MessageBuilder::new();
+            complete_signal.push("Added roles to all deviants");
+            message
+                .channel_id
+                .say(ctx, complete_signal)
+                .await
+                .expect("Could not send success message from fix_deviants");
+        }
+        _ => println!("No GuildID received in fix_deviants"),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use serenity::model::prelude::{Message, Ready, Member};
-use serenity::utils::MessageBuilder;
+use serenity::model::prelude::{Member, Message, Ready};
 use serenity::{async_trait, prelude::*};
 use serenitybot::commands;
 
@@ -8,30 +7,31 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn guild_member_addition(&self, ctx: Context, mut new_member: Member) {
-        new_member.add_role(ctx, 1126217605976436777).await.expect(stringify!("Could not add role to member with ID: {}", new_member.ID));
+        new_member
+            .add_role(ctx, 1126217605976436777)
+            .await
+            .expect(stringify!(
+                "Could not add role to member with ID: {}",
+                new_member.ID
+            ));
     }
 
     async fn message(&self, ctx: Context, message: Message) {
-        const PREFIX: &str = "?";
-            
-        if message.content.starts_with("?") {
-            let command = &*message.content.replace(PREFIX, "") ;
-            match command {
-                "ping" => commands::pong(ctx, message).await,
-                "embed" => commands::hello_embed(ctx, message).await,
-                "some" => commands::get_input(ctx, message).await,
-                "doc" => commands::get_help(ctx, message).await,
-                "8ball" => commands::eightball(ctx, message).await,
-                "showdeviants" => {
-                    if let Some(guild_id) = message.guild_id {
-                        commands::print_deviants(ctx, message.channel_id, guild_id).await;
-                    } else {
-                        let mut return_message = MessageBuilder::new();
-                        return_message.push("Could not get GuildId from Option");
+        const PREFIX: &str = "&";
 
-                        message.channel_id.say(ctx, return_message).await.expect("Could not send message about not getting a GuildId Option");
-                    }
-                },
+        if message.content.starts_with(PREFIX) {
+            let command = &*message.content.replace(PREFIX, "");
+            match command {
+                // "help" => commands::my_help(ctx, message).await,
+                "ping" => commands::pong(&ctx, &message).await,
+                "embed" => commands::hello_embed(&ctx, &message).await,
+                "some" => commands::get_input(&ctx, &message).await,
+                "doc" => commands::get_help(&ctx, &message).await,
+                "8ball" => commands::eightball(&ctx, &message).await,
+                "showdeviants" => {
+                    commands::print_deviants(&ctx, &message.channel_id, &message.guild_id).await
+                }
+                "fixdeviants" => commands::fix_deviants(&ctx, &message, &message.guild_id).await,
                 _ => (),
             }
         }
@@ -45,7 +45,9 @@ impl EventHandler for Handler {
 #[tokio::main]
 async fn main() {
     let token = std::env::var("DISCORD_TOKEN").expect("Expected a token");
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT | GatewayIntents::GUILD_MEMBERS;
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::GUILD_MEMBERS;
 
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
